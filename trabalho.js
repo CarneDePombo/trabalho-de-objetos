852207b0d171c35e7dc54d9019cd30d504802ade
Carne De Pombo <carnedpombo@gmail.com>
2025-06-05 10:51:02 +0000

    fazendo o trabalho

---
+++ trabalho.js
@@ -0,0 +1,70 @@
+let Carro = {
+    marca : 'Toyota',
+    cor : 'preto',
+    ano : 2020
+};
+
+let Cadeira = {
+    material: 'madeira',
+    cor: 'vermelha',
+    temEncosto: true
+};
+
+let Telefone = {
+    modelo: 'Iphone 13',
+    temCamera: true,
+    armazenamento: '128Gb'
+};
+
+let Relógio = {
+    tipo: 'digital',
+    marca: 'Casio',
+    resistenteÁAgua: true
+};
+
+let Fogão = {
+    bocas: 4,
+    temForno: true,
+    cor: 'branco'
+};
+
+let Ventilador = {
+    potencia: '60W',
+    velocidades: 3,
+    cor: 'azul'
+};
+
+let Mochila = {
+    compartimentos: 3,
+    cor: 'preta',
+    temAlmofadaCostas: true
+};
+
+let Copo = {
+    material: 'vidro',
+    capacidadeML: 300,
+    éDescartável: false
+};
+
+let Chave = {
+    tipo: 'residencial',
+    material: 'metal',
+    possuiEtiqueta: true
+};
+
+let Bicicleta = {
+    marchas: 21,
+    aro: 29,
+    cor: 'verde'
+}
+
+console.log(Carro.marca);
+console.log(Cadeira.cor);
+console.log(Telefone.modelo);
+console.log(Relógio.marca);
+console.log(Fogão.temForno);
+console.log(Ventilador.potencia);
+console.log(Mochila.compartimentos);
+console.log(Copo.capacidadeML);
+console.log(Chave.material);
+console.log(Bicicleta.marchas);
